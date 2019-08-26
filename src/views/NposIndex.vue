a16ad6da6c0144a4aa153855994ba8cb0890f3f9
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 15:13:17 -0500

    Added methods to NPOs Index and Show.vue

---
+++ src/views/NposIndex.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>All NPOs</h1>
+    <div v-for="npo in npos">
+      <h2>{{ npo.name }}</h2>
+      <h3>{{ npo.cause_id }}</h3>
+      <router-link v-bind:to="`/npos/${npo.id}`">More details</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      npos: []
+    };
+  },
+  created: function() {
+    axios.get("/npos").then(response => {
+      this.npos = response.data;
+      console.log(this.npos);
+    });
+  },
+  methods: {}
+};
+</script>
---
+++ src/views/NposShow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h2>{{ npo.name }}</h2>
+    <router-link to="/npos">Back to all npos</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      npo: {}
+    };
+  },
+  created: function() {
+    axios.get("/npos/" + this.$route.params.id).then(response => {
+      this.npo = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
