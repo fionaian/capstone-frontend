282727e59b0cfbf80a8b37afd02b3dbb12ce4e1d
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 15:11:57 -0500

    Added methods to Events Index and Show.vue

---
+++ src/views/EventsIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>All Events</h1>
+    <div v-for="event in events">
+      <h2>{{ event.name }}</h2>
+      <h3>{{ event.npo_id }}</h3>
+      <h3>{{ event.start_datetime }}</h3>
+      <h3>{{ event.end_datetime }}</h3>
+      <router-link v-bind:to="`/events/${event.id}`">More details about event</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      events: []
+    };
+  },
+  created: function() {
+    axios.get("/events").then(response => {
+      this.events = response.data;
+      console.log(this.events);
+    });
+  },
+  methods: {}
+};
+</script>
---
+++ src/views/EventsShow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h2>{{ event.name }}</h2>
+    <router-link to="/events">Back to all events</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      event: {}
+    };
+  },
+  created: function() {
+    axios.get("/events/" + this.$route.params.id).then(response => {
+      this.event = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
