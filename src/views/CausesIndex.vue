bbdeb2c7759719ebfe0c096f64be44d4227e6468
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 15:10:18 -0500

    Added methods to Causes Index and Show.vue

---
+++ src/views/CausesIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>All Causes</h1>
+    <div v-for="cause in causes">
+      <h2>{{ cause.title }}</h2>
+      <router-link v-bind:to="`/causes/${cause.id}`">More details</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      causes: []
+    };
+  },
+  created: function() {
+    axios.get("/causes").then(response => {
+      this.causes = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
---
+++ src/views/CausesShow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h2>{{ cause.title }}</h2>
+    <div v-for="npo in cause.npos">
+      <p>NPO: {{ npo.name }}</p>
+      <a href="">link</a>
+    </div>
+    <router-link to="/causes">Back to all causes</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      cause: {}
+    };
+  },
+  created: function() {
+    axios.get("/causes/" + this.$route.params.id).then(response => {
+      this.cause = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
