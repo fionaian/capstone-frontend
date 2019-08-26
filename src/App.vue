edc477f109eb7beafab6e65af37cf9ceb58f923a
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 15:02:51 -0500

    Added Causes, NPOs, and Events in App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@
       |
       <router-link to="/login">Login</router-link>
       |
+      <router-link to="/causes">Causes</router-link>
+      |
+      <router-link to="/npos">NPOs</router-link>
+      |
+      <router-link to="/events">Events</router-link>
+      |
       <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
