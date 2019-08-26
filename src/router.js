e3fa415cd6b969f0be007452c467d8ca8e010ffb
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 14:59:09 -0500

    Added Login.vue, router, link in app.vue

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -12,6 +13,7 @@ export default new Router({
   routes: [
     { path: "/", name: "home", component: Home },
     { path: "/about", name: "about", component: About },
-    { path: "/signup", name: "signup", component: Signup }
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login }
   ]
 });
