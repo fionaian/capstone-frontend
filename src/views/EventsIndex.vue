4aa6fc43976dbafada96f8be0f53b4c8bb346aa2
Fiona Ian <sayhitofiona@gmail.com>
2019-08-26 21:12:06 -0500

    Updated Home.vue with events for logged in users

--- src/views/EventsIndex.vue
+++ src/views/EventsIndex.vue
@@ -21,7 +21,7 @@ export default {
     };
   },
   created: function() {
-    axios.get("/api/events").then(response => {
+    axios.get("/api/all_events").then(response => {
       this.events = response.data;
       console.log(this.events);
     });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-if="!isLoggedIn">
+      <p>Extra info</p>
+    </div>
+    <div v-if="isLoggedIn">
+      <p>Your events</p>
+      <div v-for="event in events">
+        <p>{{ event.name }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to VolunTreery!",
+      events: [],
+      isLoggedIn: true
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/events").then(response => {
+      this.events = response.data;
+      console.log("data: ", this.events);
+    });
+  },
   methods: {}
 };
 </script>
