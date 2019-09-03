a6bd7fa15f9d2f27245241e468e88c46aa221537
Fiona Ian <sayhitofiona@gmail.com>
2019-09-03 18:46:05 -0500

    Added twitter_url component

--- src/views/NposShow.vue
+++ src/views/NposShow.vue
@@ -5,6 +5,10 @@
       <h3>Event: {{ event.name }}</h3>
       <h4>Cause: {{ npo.cause.title }}</h4>
       <router-link v-bind:to="`/events/${event.id}`">Additional Information about {{ event.name }}</router-link>
+      <br />
+      <twitter :key="componentKey" style="width: 50%; height: 50%">
+        <a class="twitter-timeline" :href="twitterUrl">Tweets by {{ npo.name }}</a>
+      </twitter>
     </div>
     <router-link to="/npos">Back to all npos</router-link>
   </div>
@@ -12,16 +16,29 @@
 
 <script>
 import axios from "axios";
+import { twitter } from "vue-twitter";
 export default {
+  components: {
+    twitter
+  },
   data: function() {
     return {
+      componentKey: 0,
       npo: {},
-      event: []
+      event: [],
+      twitterUrl: ""
     };
   },
   created: function() {
     axios.get("/api/npos/" + this.$route.params.id).then(response => {
       this.npo = response.data;
+      // setCurrentProfile: function(profile) {
+      // this.currentProfile = profile;
+      // this.twitterUrl = "https://twitter.com/" + this.currentProfile.twitter_handle + "?ref_src=twsrc%5Etfw";
+      // this.twitterUrl = "https://twitter.com/twitterdev";
+      this.twitterUrl = this.npo.twitter_url;
+      // this.twitterUrl = "https://twitter.com/twitterdev?ref_src=twsrc%5Etfw";
+      this.componentKey += 1;
     });
   },
   methods: {}
