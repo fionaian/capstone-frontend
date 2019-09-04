8e0c56e1cda115ec5fa0e09f5af7cc8fd2ed285a
Fiona Ian <sayhitofiona@gmail.com>
2019-09-03 20:46:06 -0500

    Added images to causes

--- src/views/CausesIndex.vue
+++ src/views/CausesIndex.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="container">
     <h1>All Causes</h1>
-    <div v-for="cause in causes">
-      <h2>{{ cause.title }}</h2>
-      <router-link v-bind:to="`/causes/${cause.id}`">More details</router-link>
-    </div>
 
     <section class="bg-alt">
       <div class="container py-5">
@@ -13,37 +9,106 @@
             <!-- card -->
             <div class="card card-default text-center">
               <div class="card-img-top card-zoom">
-                <a href="blog-detail.html">
-                  <img class="img-fluid" src="assets/images/mercantile_500_0002.png" alt="Card image" />
+                <a href="/causes/1">
+                  <img
+                    class="img-fluid"
+                    src="https://icon-library.net/images/education-icon/education-icon-23.jpg"
+                    alt="Card image"
+                  />
                 </a>
               </div>
               <div class="card-body">
-                <h6>Bring It</h6>
-                <p class="card-text">It&apos;s ok if there&apos;s more.</p>
+                <h6>Education</h6>
+                <p class="card-text">Teach and mentor.</p>
               </div>
             </div>
             <!-- card -->
             <div class="card card-default text-center">
               <div class="card-img-top card-zoom">
-                <a href="blog-detail.html">
-                  <img class="img-fluid" src="assets/images/mercantile_500_0003.png" alt="Card image" />
+                <a href="/causes/2">
+                  <img
+                    class="img-fluid"
+                    src="https://marketplace.canva.com/MADHh7rHHyg/1/thumbnail_large/canva-woman-with-mobile-social-media-icons-MADHh7rHHyg.png"
+                    alt="Card image"
+                  />
                 </a>
               </div>
               <div class="card-body">
-                <h6>Elegance</h6>
-                <p class="card-text">Some clever wordage.</p>
+                <h6>Media</h6>
+                <p class="card-text">Tap your creative side.</p>
               </div>
             </div>
             <!-- card -->
             <div class="card card-default text-center">
               <div class="card-img-top card-zoom">
-                <a href="blog-detail.html">
-                  <img class="img-fluid" src="assets/images/mercantile_500_0006.png" alt="Card image" />
+                <a href="/causes/3">
+                  <img
+                    class="img-fluid"
+                    src="https://dghscp.co.uk/wp-content/uploads/2018/10/Population-icon.png"
+                    alt="Card image"
+                  />
                 </a>
               </div>
               <div class="card-body">
-                <h6>Design It</h6>
-                <p class="card-text">More clever wordage.</p>
+                <h6>Social Services</h6>
+                <p class="card-text">Care for all.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-alt">
+      <div class="container py-5">
+        <div class="mt-2 wow fadeIn">
+          <div class="card-deck">
+            <!-- card -->
+            <div class="card card-default text-center">
+              <div class="card-img-top card-zoom">
+                <a href="/causes/4">
+                  <img
+                    class="img-fluid"
+                    src="https://cdn2.iconfinder.com/data/icons/travel-caramel-vol-3/256/THEATRE-512.png"
+                    alt="Card image"
+                  />
+                </a>
+              </div>
+              <div class="card-body">
+                <h6>Performing Arts</h6>
+                <p class="card-text">Express Yourself.</p>
+              </div>
+            </div>
+            <!-- card -->
+            <div class="card card-default text-center">
+              <div class="card-img-top card-zoom">
+                <a href="/causes/5">
+                  <img
+                    class="img-fluid"
+                    src="http://pluspng.com/img-png/pet-care-png-emergency-pet-care-buckwalter-veterinary-clinic-bluffton-sc-650.png"
+                    alt="Card image"
+                  />
+                </a>
+              </div>
+              <div class="card-body">
+                <h6>Animal Care</h6>
+                <p class="card-text">Helping animals, one paw at a time.</p>
+              </div>
+            </div>
+            <!-- card -->
+            <div class="card card-default text-center">
+              <div class="card-img-top card-zoom">
+                <a href="/causes/6">
+                  <img
+                    class="img-fluid"
+                    src="https://images.assetsdelivery.com/compings_v2/vgarts/vgarts1506/vgarts150601440.jpg"
+                    alt="Card image"
+                  />
+                </a>
+              </div>
+              <div class="card-body">
+                <h6>Human Rights</h6>
+                <p class="card-text">Stand up for all.</p>
               </div>
             </div>
           </div>
