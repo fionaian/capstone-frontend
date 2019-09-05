be62b3ad11a5ea9c9fbe0f70707e21b4ee91e388
Fiona Ian <sayhitofiona@gmail.com>
2019-09-05 12:59:05 -0500

    Updated About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,6 +13,159 @@
             </p>
           </div>
           <router-link to="/causes"><i>Go to Causes</i></router-link>
+          <br />
+
+          <div class="bg-alt wow slideInUp" data-wow-delay="1.2s">
+            <div class="container py-5">
+              <!-- alternating left right testimonials list -->
+              <div class="row my-5">
+                <div class="col-12">
+                  <div class="row">
+                    <div class="col-12 col-md-7">
+                      <img
+                        class="rounded-circle float-md-right d-block mx-auto mx-md-3"
+                        src="https://www.aauw.org/files/2012/07/leslie-knope-badge-756x756.jpg?w=300"
+                        height="128px"
+                        width="128px"
+                        alt="client image"
+                      />
+                      <blockquote class="text-md-right text-center">
+                        <a href="#" class="link lead">Leslie Knope</a>
+                        <br />
+                        <span class="small">
+                          Education Enthusiast
+                          <br />
+                          Fellow Voluntreer
+                        </span>
+                      </blockquote>
+                    </div>
+                    <div class="col-12 col-md-5">
+                      <div class="card card-default">
+                        <div class="container-fluid">
+                          <div class="row">
+                            <div class="col-sm-12 py-2">
+                              <h5>&quot;Get Involved!&quot;</h5>
+                              <p>
+                                I love VolunTreery! I get to find all these great community volunteering opportunities
+                                and meet great people!
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr class="my-4" />
+                  <div class="row">
+                    <div class="col-md-7 order-md-2 col-12">
+                      <img
+                        class="rounded-circle float-md-left d-block mx-auto mx-md-3"
+                        src="https://i.pinimg.com/originals/60/95/6c/60956c940aefba18f61a226ad37e2cf1.jpg"
+                        height="128px"
+                        width="128px"
+                        alt="client image"
+                      />
+                      <blockquote class="text-md-left text-center">
+                        <a href="#" class="link lead">Ann Perkins</a>
+                        <br />
+                        <span class="small">
+                          Social Service Organizer
+                          <br />
+                          Fellow Voluntreer
+                        </span>
+                      </blockquote>
+                    </div>
+                    <div class="col-md-5 order-md-1 col-12">
+                      <div class="card card-default">
+                        <div class="container-fluid">
+                          <div class="row">
+                            <div class="col-sm-12 py-2">
+                              <h5>&quot;Great Network&quot;</h5>
+                              <p>
+                                VolunTreery is your one-stop shop for a great network of do-gooders!
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr class="my-4" />
+                  <div class="row">
+                    <div class="col-12 col-md-7">
+                      <img
+                        class="rounded-circle float-md-right d-block mx-auto mx-md-3"
+                        src="https://img.nbc.com/sites/nbcunbc/files/files/images/2013/11/08/retta_donnaMeagle.jpg"
+                        height="128px"
+                        width="128px"
+                        alt="client image"
+                      />
+                      <blockquote class="text-md-right text-center">
+                        <a href="#" class="link lead">Donna Meagle</a>
+                        <br />
+                        <span class="small">
+                          Media Maven
+                          <br />
+                          Fellow Voluntreer
+                        </span>
+                      </blockquote>
+                    </div>
+                    <div class="col-12 col-md-5">
+                      <div class="card card-default">
+                        <div class="container-fluid">
+                          <div class="row ">
+                            <div class="col-sm-12 py-2">
+                              <h5>&quot;Absolutely the best&quot;</h5>
+                              <p>
+                                This is handsdown the best site to find out how you can give back to your community.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr class="my-4" />
+                  <div class="row">
+                    <div class="col-md-7 order-md-2 col-12">
+                      <img
+                        class="rounded-circle float-md-left d-block mx-auto mx-md-3"
+                        src="https://img.nbc.com/sites/nbcunbc/files/files/images/2013/11/08/aubreyPlaza_aprilLudgate.jpg"
+                        height="128px"
+                        width="128px"
+                        alt="client image"
+                      />
+                      <blockquote class="text-md-left text-center">
+                        <a href="#" class="link lead">April Ludgate</a>
+                        <br />
+                        <span class="small">
+                          Dog Whisperer
+                          <br />
+                          Fellow Voluntreer
+                        </span>
+                      </blockquote>
+                    </div>
+                    <div class="col-md-5 order-md-1 col-12">
+                      <div class="card card-default">
+                        <div class="container-fluid">
+                          <div class="row">
+                            <div class="col-sm-12 py-2">
+                              <h5>&quot;Pretty Awesome&quot;</h5>
+                              <p>
+                                I was surprised that this was so intuitive in getting me to connect with the causes I
+                                care about.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--/container-->
+          </div>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-3"></div>
       </div>
