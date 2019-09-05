cf5de00c8638e1f382742789228030cdc6229759
Fiona Ian <sayhitofiona@gmail.com>
2019-09-05 09:06:57 -0500

    Edited NposShow.vue

--- src/views/NposShow.vue
+++ src/views/NposShow.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="container">
-    <h2>Events from {{ npo.name }}</h2>
-    <div v-for="event in npo.events">
-      <h3>Event: {{ event.name }}</h3>
-      <h4>Cause: {{ npo.cause.title }}</h4>
-      <router-link v-bind:to="`/events/${event.id}`">Additional Information about {{ event.name }}</router-link>
-      <br />
-      <twitter :key="componentKey" style="width: 50%">
-        <a class="twitter-timeline" :href="twitterUrl">Tweets by {{ npo.name }}</a>
-      </twitter>
+  <section id="aboutheader" class="bg-image-jungle bg-fixed bg-cover">
+    <div class="container pt-2">
+      <div class="row">
+        <div class="col-xl-7 col-lg-8 col-md-9 bg-white p-4 mx-1 wow fadeInUp hide">
+          <div class="pt-2 pb-2">
+            <h2>Events from {{ npo.name }}</h2>
+            <div v-for="event in npo.events">
+              <h3>Event: {{ event.name }}</h3>
+              <h4>Cause: {{ npo.cause.title }}</h4>
+              <router-link v-bind:to="`/events/${event.id}`">Additional Information about {{ event.name }}</router-link>
+              <br />
+              <twitter :key="componentKey" style="width: 50%">
+                <a class="twitter-timeline" :href="twitterUrl">Tweets by {{ npo.name }}</a>
+              </twitter>
+            </div>
+            <router-link to="/npos"><i>Back to all npos</i></router-link>
+          </div>
+          <div class="col-xl-4 col-lg-4 col-md-3"></div>
+        </div>
+      </div>
     </div>
-    <router-link to="/npos"><i>Back to all npos</i></router-link>
-  </div>
+  </section>
 </template>
 
 <script>
