28235f59a7e573fc24ae67cf1721c61c4ae88dc0
Fiona Ian <sayhitofiona@gmail.com>
2019-09-04 23:26:36 -0500

    About.vue edited

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section id="aboutheader" class="bg-image2">
+    <div class="container pt-2">
+      <div class="row">
+        <div class="col-xl-7 col-lg-8 col-md-9 bg-white p-4 mx-1 wow fadeInUp hide">
+          <div class="pt-2 pb-2">
+            <h1 class="">We Connect You to Good Causes</h1>
+            <h6 class="wow fadeIn text-uppercase small text-bold wide-space">It&apos;s Our Mission</h6>
+            <p class="lead pt-2 pr-2">
+              At VolunTreery, we strive to connect our volunteers to meaningful social causes. The idea is for each
+              volunteer to grow their own community of do-gooders. Like a tree, each volunteer will tend their roots by
+              branching out to explore non-profit volunteering opportunities in their area.
+            </p>
+          </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-3"></div>
+      </div>
+    </div>
+  </section>
 </template>
